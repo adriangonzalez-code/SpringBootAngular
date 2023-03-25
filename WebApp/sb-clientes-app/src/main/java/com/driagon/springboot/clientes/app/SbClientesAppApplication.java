28c82980e0fb66549bd2d0e4b110c314151290dc
Adrian Gonzalez <g.adrian.gonzalez.90@gmail.com>
2023-03-24 22:22:21 -0600

    Added rest application

---
+++ WebApp/sb-clientes-app/src/main/java/com/driagon/springboot/clientes/app/SbClientesAppApplication.java
@@ -0,0 +1,13 @@
+package com.driagon.springboot.clientes.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbClientesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbClientesAppApplication.class, args);
+    }
+
+}
